3e50c3cf082b53a456d39bb582a5596819ad845e
Muskaan Joshi <111475258+JoshiMuskaan@users.noreply.github.com>
2025-03-07 20:38:29 -0600

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `IB Interview StudyIBuddy`;
 export const MESSAGE_PLACEHOLDER: string = `Ask a question!`;
 export const FOOTER_MESSAGE: string = `AI assistant by MJ`;
 export const CLEAR_BUTTON_TEXT: string = `Reset`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Chat with your AI Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
