a9cfb85cc7583fcc2fdb807a85a311d1f711b805
Muskaan Joshi <111475258+JoshiMuskaan@users.noreply.github.com>
2025-03-07 20:50:45 -0600

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Muskaan Joshi`;
 export const OWNER_DESCRIPTION: string = `A Business student at UNC Chapel Hill!`;
 
-export const AI_NAME: string = `StudyBuddy`;
+export const AI_NAME: string = `StudyIBuddy`;
 export const AI_TONE: string = `Encouraging, Clear, and Engaging`;
 export const AI_ROLE: string = `A helpful interview study assistant specializing in finance, business, and investment banking.`;
